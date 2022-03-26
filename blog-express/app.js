e455e23a7818380dc1f08766fdbe0d50515dce03
Hedyjing <374884275@qq.com>
2022-03-26 16:13:23 +0800

    express router

--- blog-express/app.js
+++ blog-express/app.js
@@ -4,23 +4,34 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// 引入路由， 对应blog1中app.js的引入路由
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const blogRouter = require('./routes/blog')
+const userRouter = require('./routes/user')
 
+// 给每一个请求创建一个express实例
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// 对应blog1中的日志access()
 app.use(logger('dev'));
+// 下面两行对应blog1中的app.js处理post data, 这样就可以在req.body直接访问post数据了
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// 对应blog1中解析cookie， 可以通过req.cookie访问
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 注册路由
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/blog', blogRouter)
+app.use('/api/user', userRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -31,7 +42,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'dev' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
