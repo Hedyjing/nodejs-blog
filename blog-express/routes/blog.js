33199900aa5522dc4015e14c7907513f2bc77fee
Hedyjing <374884275@qq.com>
2022-03-26 23:18:17 +0800

    利用loginCheck完成所有路由的登录校验

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -2,24 +2,48 @@ var express = require('express');
 var router = express.Router();
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 const { getList, getDetail, newBlog, updateBlog, deleteBlog } = require('../controller/blog')
-
+const loginCheck = require('../middleware/loginCheck')
 
 router.get('/list', function (req, res, next) {
   const author = req.query.author || ''
   const keyword = req.query.keyword || ''
-  // const loginCheckResult = loginCheck(req)
-  // if (loginCheckResult) {
-  //   return loginCheckResult
-  // }
+
   return getList(author, keyword).then(listData => {
     res.json(new SuccessModel(listData))
   })
 });
 
 router.get('/detail', function (req, res, next) {
-  res.json({
-    errno: 0,
-    data: 'OK'
+  return getDetail(id).then(data => {
+    res.json(new SuccessModel(data))
+  })
+})
+
+router.post('/new', loginCheck, (req, res, next) => {
+  req.body.author = req.session.username
+  return newBlog(req.body).then(data => {
+    res.json(new SuccessModel(data))
+  })
+})
+
+router.post('/update', loginCheck, (req, res, next) => {
+  const result = updateBlog(req.query.id || 0, req.body)
+  return result.then(result => {
+    if (result) {
+      res.json(new SuccessModel())
+    } else {
+      res.json(new ErrorModel('更新博客失败'))
+    }
+  })
+})
+
+router.post('/del', loginCheck, (req, res, next) => {
+  return deleteBlog(req.query.id || 0, req.session.username).then(result => {
+    if (result) {
+      res.json(new SuccessModel())
+    } else {
+      res.json(new ErrorModel('删除博客失败'))
+    }
   })
 })
 
